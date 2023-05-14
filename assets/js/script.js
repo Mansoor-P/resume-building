6261ed325735b4b6f82fb1c7811f614dced6c73a
MansoorP <103396101+Mansoor-P@users.noreply.github.com>
2023-05-14 11:31:37 +0530

    toggle button introdused

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 
 
 // form repeater
-$(document).ready(function(){
+$(document).ready(function () {
     $('.repeater').repeater({
         initEmpty: false,
         defaultValues: {
             'text-input': ''
         },
-        show:function(){
+        show: function () {
             $(this).slideDown();
         },
-        hide: function(deleteElement){
+        hide: function (deleteElement) {
             $(this).slideUp(deleteElement);
             setTimeout(() => {
                 generateCV();
@@ -19,3 +19,16 @@ $(document).ready(function(){
         isFirstItemUndeletable: true
     })
 })
+
+
+
+
+  /* Open when someone clicks on the span element */
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
